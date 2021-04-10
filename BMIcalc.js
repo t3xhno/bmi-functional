3db5af0a10812da3b3ce97b0ffcf566d5576bf81
t3x <t3xhnolainx@gmail.com>
2021-04-10 13:45:40 +0200

    Added timing utilities

--- BMIcalc.js
+++ BMIcalc.js
@@ -1,3 +1,6 @@
+// Import the dataset
+const datasets = require('./data');
+
 // Defining map/filter implemented through reduce,
 // compose and transduce functions
 const redFn = (acc, x) => acc.concat(x);
@@ -5,26 +8,25 @@ const mapping = fn => reducer => (acc, x) => reducer(acc, fn(x));
 const filtering = pred => reducer => (acc, x) => pred(x) ? reducer(acc, x) : acc;
 const compose = (...fns) => arg => fns.reduceRight((acc, fn) => fn(acc), arg);
 const transduce = (xform, reducer, init, data) => data.reduce(xform(reducer), init);
-// Define the dataset
-const dataset = [
-    {name: 'Marko', weight: 70, height: 1.9},
-    {name: 'Jovan', weight: 50, height: 1.7},
-    {name: 'Luka', weight: 40, height: 1.85},
-    {name: 'Janko', weight: 65, height: 1.6},
-    {name: 'Miloje', weight: 55, height: 1.65},
-    {name: 'Sofija', weight: 120, height: 2.12},
-    {name: 'Biljana', weight: 100, height: 1.83},
-    {name: 'Nemanja', weight: 92, height: 1.81},
-    {name: 'Arijan', weight: 78, height: 1.65},
-    {name: 'Nikola', weight: 98, height: 1.5},
-];
+
+// Desctucture console
+const { log } = console;
+
+// Time measuring utilities
+const tElapsed = ([s, ns]) => `Execution time: ${(s * 1000 + ns / 1000000).toFixed(3)} ms.`;
+const timeIt = fn => (...args) => {
+    const start = process.hrtime();
+    const output = fn(...args);
+    log(tElapsed(process.hrtime(start)));
+    return output;
+};
+
 // Define filters and maps needed
 const getBMI = ({ height: h, weight: w, ...rest }) => ({ bmi: w / h ** 2, ...rest });
-const roundDec = ({ bmi, ...rest }) => ({ bmi: bmi.toFixed(2), ...rest });
+const roundDec = places => ({ bmi, ...rest }) => ({ bmi: bmi.toFixed(places), ...rest });
 const isObese = ({ bmi, ...rest }) => bmi > 25.0;
 const formatOutput = ({ name, bmi }) => `${name} has a BMI of ${bmi}, which indicates obesity.`;
+
 // Put them together in an xform composer, and run it
-const xform = compose(mapping(getBMI), mapping(roundDec), filtering(isObese), mapping(formatOutput));
-const start = process.hrtime();
-const outputData = transduce(xform, redFn, [], dataset);
-console.log(`Execution time: ${process.hrtime(start)[0] + process.hrtime(start)[1] / 1000000}s.`);
+const xform = compose(mapping(getBMI), mapping(roundDec(2)), filtering(isObese), mapping(formatOutput));
+log(timeIt(transduce)(xform, redFn, [], datasets.data));
---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+    {name: 'Marko', weight: 70, height: 1.9},
+    {name: 'Jovan', weight: 50, height: 1.7},
+    {name: 'Luka', weight: 40, height: 1.85},
+    {name: 'Janko', weight: 65, height: 1.6},
+    {name: 'Miloje', weight: 55, height: 1.65},
+    {name: 'Sofija', weight: 120, height: 2.12},
+    {name: 'Biljana', weight: 100, height: 1.83},
+    {name: 'Nemanja', weight: 92, height: 1.81},
+    {name: 'Arijan', weight: 78, height: 1.65},
+    {name: 'Nikola', weight: 98, height: 1.5}
+];
+
+module.exports = { data };
